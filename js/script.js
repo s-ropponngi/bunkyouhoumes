aeecb883d6f3ed10a1a800bebd7e631bc01037bb
tomomi sugawara <>
2022-07-11 00:55:13 +0900

    7/10

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 
+// +++++ 物件スライダー +++++
+$('.p-info-slider__in').slick({
+  autoplay: true, //自動再生
+  autoplaySpeed: 1000, //自動再生のスピード
+  speed: 800, //スライドするスピード
+  dots: false, //スライドしたのドット
+  arrows: true, //左右の矢印
+  // infinite: true, //スライドのループ
+  // pauseOnHover: false, //ホバーしたときにスライドを一時停止しない
+  // fade: true,
+  slidesToShow: 4,
+  // centerMode: true,
+  variablewidth: true
+});
+
+// +++++ アクセススライダー +++++
 $('.p-access-slide__in').slick({
   autoplay: true, //自動再生
   autoplaySpeed: 1000, //自動再生のスピード
@@ -8,7 +24,18 @@ $('.p-access-slide__in').slick({
   // infinite: true, //スライドのループ
   // pauseOnHover: false, //ホバーしたときにスライドを一時停止しない
   // fade: true,
-  slidesToShow:4,
+  slidesToShow: 4,
   // centerMode: true,
-  variablewidth:true
+  variablewidth: true
+});
+
+// +++++ Animation in +++++
+$(function () {
+  $('.slide-in-right').on('inview', function (event, isInView) {
+    if (isInView) {
+      //表示領域に入った時
+      $(this).addClass('fadeInRight animated');
+      $(this).css('opacity', '1');
+    }
+  });
 });
